1de729936d44033a185f9ceca789866eef376ce9
Ifeoluwa <iamsamnice@gmail.com>
2025-10-29 01:10:21 +0100

    Login page UI for all screens

--- login.php
+++ login.php
@@ -13,50 +13,68 @@
 </head>
 <body>
     <form action="includes/loginFormhandler.inc.php" method="post">
-        <div class="w-full h-screen border bg-black p-3 flex items-end justify-end">
-            <!-- <h1 class="text-blue-500 text-xl">LOGIN</h1> -->
-            <div class="w-1/2 p-3 bg-zinc-50 h-full rounded-md flex justify-center items-center">
-                <div class="w-3/4 h-3/4 flex flex-col gap-8">
+        <div class="w-full h-screen border bg-black p-1 lg:p-3 flex items-end justify-end">
+            <div class="w-full lg:w-1/2 p-3 bg-zinc-50 h-full rounded-md flex justify-center items-center">
+                <div class="w-11/12 lg:w-3/4 flex flex-col gap-8">
                     <div>
                         <h1 class=" text-4xl font-bold">Welcome!</h1>
                         <p class=" text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                     </div>
                     <div class="w-full flex flex-col gap-4">
-                        <div class="flex flex-col gap-2">
-                            <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 rounded-md p-3">
+                        <div class="flex flex-col">
+                            <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 p-3">
                                 <?php renderIcon('email', 'w-6 h-6') ?>
                                 <input 
                                     type="text" name="email" 
-                                    placeholder="Email" 
+                                    placeholder="Email"
+                                    required
                                     class="w-full font-semibold outline-none <?= 
                                         (isset($_GET['error']) && ($_GET['error'] === 'invalidcredentials' || $_GET['error'] === 'emptyfields')) ? 'border-red-500' : 'border-black' 
                                     ?>" 
                                 />
-                                    <?php 
-                                        if (isset($_GET['error']) && $_GET['error'] === 'emptyfields') { echo '<p class="text-red-500 text-xs">This field is empty</p>';}
-                                    ?>
                             </div>
+                            <?php 
+                                if (isset($_GET['error']) && ($_GET['error'] === 'emptyfields')) { echo '
+                                    <p class="text-red-500 text-xs self-end">
+                                        This field is empty
+                                    </p>'; 
+                                }
+                            ?> 
                         </div>
-                        <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 rounded-md p-3">
-                            <select name="role" class="w-full outline-none">
-                                <option value="">What's your role?</option>
-                                <option value="student">Student</option>
-                                <option value="teacher">Teacher</option>
-                            </select>
+                            <div class="flex flex-col">
+                                <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 p-3">
+                                <select name="role" class="w-full outline-none" required>
+                                    <option value="">What's your role?</option>
+                                    <option value="student">Student</option>
+                                    <option value="teacher">Teacher</option>
+                                </select>
+                            </div>
+                            <?php 
+                                if (isset($_GET['error']) && ($_GET['error'] === 'emptyfields') || $role === '') { echo '
+                                    <p class="text-red-500 text-xs self-end">
+                                        This field is empty
+                                    </p>'; 
+                                }
+                            ?>
                         </div>
-                        <div class="flex flex-col gap-2">
-                            <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 rounded-md p-3">
+                        <div class="flex flex-col">
+                            <div class="w-full flex gap-2 items-center border-b-2 border-zinc-400 p-3">
                                 <?php renderIcon('password', 'w-6 h-6') ?>
                                 <input 
                                     type="password" 
                                     name="pwd" 
                                     placeholder="Password" 
+                                    required
                                     class="w-full font-semibold outline-none <?= (isset($_GET['error']) && ($_GET['error'] === 'invalidcredentials' || $_GET['error'] === 'emptyfields')) ? 'border-red-500' : 'border-black' ?>" 
                                 />
-                                <?php 
-                                    if (isset($_GET['error']) && ($_GET['error'] === 'emptyfields')) { echo '<p class="text-red-500 text-xs">This field is empty</p>'; } 
-                                ?>
                             </div>
+                            <?php 
+                                if (isset($_GET['error']) && ($_GET['error'] === 'emptyfields')) { echo '
+                                    <p class="text-red-500 text-xs self-end">
+                                        This field is empty
+                                    </p>'; 
+                                }
+                            ?> 
                         </div>
                     </div>
                     <div class="flex flex-col gap-2 justify-end items-end mt-2">
