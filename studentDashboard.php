21f56b34bf7b3ab532bbb78a6eaab62b0f1e2425
Ifeoluwa <iamsamnice@gmail.com>
2025-11-04 12:15:52 +0100

    Student Dashboard UI

--- studentDashboard.php
+++ studentDashboard.php
@@ -59,7 +59,7 @@ function getSelectedSubjects($conn, $student_id) {
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> 
     <title>Student Dashboard</title>
 </head>
-<body class="bg-zinc-200/60">
+<body class="bg-neutral-50">
     <header class="w-full border-b">
         <div class="w-full mx-auto">
             <div class="mx-auto w-11/12 lg:w-10/12 flex justify-between p-2">
@@ -67,7 +67,7 @@ function getSelectedSubjects($conn, $student_id) {
                     <img src="public\png-aura.com.png" class="w-10 h-10" />
                     <h1 class="font-bold text-3xl">SCHOOL NAME</h1>
                 </div>
-                <a href="studentProfile.php?id=<?= $id ?>" class="hidden lg:flex flex-col justify-end items-end cursor-pointer p-2 hover:bg-zinc-300 duration-300 transition-all">
+                <a href="studentProfile.php?id=<?= $id ?>" class="hidden md:flex flex-col justify-end items-end cursor-pointer p-2 hover:bg-zinc-300 duration-300 transition-all">
                     <h2 class="font-semibold text-xl"><?= htmlspecialchars($_SESSION['student_name']) ?></h2>
                     <p class="text-zinc-400 text-sm -mt-1"><?= htmlspecialchars($_SESSION['role']) ?></p>
                 </a>
@@ -76,13 +76,13 @@ function getSelectedSubjects($conn, $student_id) {
     </header>
     <main class="w-full min-h-[700px] p-2 mx-auto">
         <section class="mx-auto w-11/12 lg:w-10/12 flex flex-col lg:flex-row lg:gap-4 gap-2">
-            <div class="w-full lg:w-9/12 lg:h-48 bg-blue-500/90 rounded-md p-3 lg:pt-3 lg:p-5 flex justify-center items-center">
+            <div class="w-full lg:w-9/12 md:h-38 lg:h-48 bg-blue-500/90 rounded-md p-3 lg:pt-3 lg:p-5 flex justify-center items-center">
                 <div class="w-full h-full flex flex-col justify-between gap-4 lg:gap-0">
                     <div>
                         <p class="text-xl lg:text-2xl font-semibold text-zinc-200">Lorem, ipsum dolor.</p>
                         <p class="text-xs lg:text-sm text-zinc-300 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, nisi sequi tenetur possimus veniam beatae.</p>
                     </div>
-                    <div class="flex flex-col lg:flex-row gap-2 lg:gap-28">
+                    <div class="flex flex-col md:flex-row gap-2 md:gap-x-14 lg:gap-28">
                         <div class="flex lg:justify-center lg:items-center gap-2">
                             <div class="w-10 h-10 border rounded-full flex justify-center items-center">
                                 <?php renderIcon('person', 'w-6 h-6') ?>
@@ -124,7 +124,7 @@ function getSelectedSubjects($conn, $student_id) {
                         x-text="time" 
                         class="w-full flex-1 text-neutral-900 text-center font-semibold pt-1"></p>
                 </div>
-                <div class="bg-white h-20 w-full rounded hover:shadow-2xl duration-300 transition-all p-2 flex items-center gap-2">
+                <div class="bg-white h-20 w-full rounded hover:shadow-2xl duration-300 transition-all p-2 flex items-center gap-2 border-zinc-200/65 border">
                     <div class="w-20 bg-amber-400 rounded h-full flex justify-center items-center">
                         <?php renderIcon('sessionStatus', 'w-8 h-8 text-white') ?>
                     </div>
@@ -137,7 +137,7 @@ function getSelectedSubjects($conn, $student_id) {
         </section>
         <section class="mx-auto w-11/12 lg:w-10/12 mt-5">
             <h2 class="font-bold text-xl pl-5 mb-2">Daily Assesment</h2>
-            <div class="w-full flex flex-col lg:flex-row gap-4">
+            <div class="w-full flex flex-col md:flex-row gap-4">
                 <div class="w-full lg:w-1/2 h-32 bg-white flex flex-col lg:flex-1 rounded-b-xl shadow-gray-950">
                     <div class="w-full p-2 flex justify-center items-center bg-blue-300/30 text-blue-500/80 font-semibold text-sm shadow-black">Assigment</div>
                     <p class="w-full flex-1 text-neutral-900 text-center font-semibold pt-5">You don't have any assignment at this time.</p>
@@ -151,11 +151,11 @@ function getSelectedSubjects($conn, $student_id) {
             </div>
         </section>
         <section class="mx-auto w-11/12 lg:w-10/12 mt-5">
-            <div class="w-full flex flex-col lg:flex-row gap-4">
+            <div class="w-full flex flex-col md:flex-row gap-4">
                 <div class="w-full lg:w-1/2">
                     <h3 class="font-bold text-xl pl-5 mb-2">Term Assesment</h3>
                     <div class="w-full">
-                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all">
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
                             <div class="h-full w-20 rounded-md flex justify-center items-center bg-indigo-600">
                                 <?php renderIcon('term', 'w-8 h-8 text-white') ?>
                             </div>
@@ -164,18 +164,54 @@ function getSelectedSubjects($conn, $student_id) {
                                 <p class="text-neutral-900 font-semibold">First Term</p>
                             </div>
                         </div>
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
+                            <div class="h-full w-20 rounded-md flex justify-center items-center bg-fuchsia-500">
+                                <?php renderIcon('subjects', 'w-8 h-8 text-white') ?>
+                            </div>
+                            <div>
+                                <p class="text-sm text-zinc-400 font-semibold">Offered Subjects</p>
+                                <p class="text-neutral-900 font-semibold">You are Offering 12 subjects. <span><a href="" class="text-blue-400 border-dotted border-b-blue-400 border-b cursor-pointer">View Offered subjects</a></span></p>
+                            </div>
+                        </div>
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
+                            <div class="h-full w-20 rounded-md flex justify-center items-center bg-sky-600">
+                                <?php renderIcon('fees', 'w-8 h-8 text-white') ?>
+                            </div>
+                            <div>
+                                <p class="text-sm text-zinc-400 font-semibold">School Fees</p>
+                                <p class="text-neutral-900 font-semibold">Paid. <span><a href="" class="text-blue-400 border-dotted border-b-blue-400 border-b cursor-pointer">View details</a></span></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="w-full lg:w-1/2">
                     <h3 class="font-bold text-xl pl-5 mb-2">Session Assesment</h3>
                     <div class="w-full">
-                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all">
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
                             <div class="h-full w-20 rounded-md flex justify-center items-center bg-teal-600">
                                 <?php renderIcon('session', 'w-8 h-8 text-white') ?>
                             </div>
                             <div>
                                 <p class="text-sm text-zinc-400 font-semibold">Current Session</p>
                                 <p class="text-neutral-900 font-semibold">2025/2026 Session</p>
+                            </div>
+                        </div>
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
+                            <div class="h-full w-20 rounded-md flex justify-center items-center bg-sky-400">
+                                <?php renderIcon('grade', 'w-8 h-8 text-white') ?>
+                            </div>
+                            <div>
+                                <p class="text-sm text-zinc-400 font-semibold">Current class</p>
+                                <p class="text-neutral-900 font-semibold"><?= htmlspecialchars($className) ?></p>
+                            </div>
+                        </div>
+                        <div class="w-full h-24 bg-white rounded-md p-3 flex items-center gap-4 hover:shadow-2xl duration-300 transition-all mb-4 border-zinc-200/65 border">
+                            <div class="h-full w-20 rounded-md flex justify-center items-center bg-emerald-600">
+                                <?php renderIcon('termsCompleted', 'w-8 h-8 text-white') ?>
+                            </div>
+                            <div>
+                                <p class="text-sm text-zinc-400 font-semibold">Terms Completed in 2025/26 Session</p>
+                                <p class="text-neutral-900 font-semibold">First, Second Term</p>
                             </div>
                         </div>
                     </div>
