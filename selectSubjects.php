53fadb88df0d27744ad97522ece9c539a60d520c
Ifeoluwa <iamsamnice@gmail.com>
2025-11-05 09:11:27 +0100

    [FEAT]: LogOut Dialog Box Ui

--- selectSubjects.php
+++ selectSubjects.php
@@ -69,17 +69,19 @@ function getSelectedSubjects($conn, $student_id) {
                     </form>
                 </div>
             </div>
-            <div x-data="{ open = false }">
+            <div x-data="{ open: false }" class="mx-auto">
                 <p class="mx-auto">Not Sure about subjects? <span><a x-on:click="open = ! open" class="cursor-pointer font-semibold text-blue-400 border-dotted border-b-blue-400 border-b">Log Out</a>, while you confirm.</span></p>
-                <div x-show="open" class="absolute top-0 left-0 w-full h-screen flex justify-center items-center">
-                    <div class="flex flex-col items-center w-60 h-40 border rounded-md p-3">
-                        <h3 class="text-xl">Log out.</h3>
-                        <div class="flex flex-col justify-center items-center">
-                            <p class="text-2xl font-semibold mt-3">Are You Sure?</p>
-                            <div class="flex gap-4 mt-1">
-                                <a href="logout.php" class="border px-4 rounded">Yes</a>
-                                <a x-on:click="open = ! open" href="#" class="border px-4 rounded">No</a>
-                            </div>
+            </div>
+        </div>
+        <div x-show="open" class="bg-zinc-100/20 absolute top-0 left-0 w-full flex justify-center items-center backdrop-blur-sm p-5">
+            <div class="bg-zinc-200/40 w-2/5 flex justify-center items-center p-5 rounded-4xl backdrop-blur-md border-zinc-100 border">
+                <div class="flex flex-col items-center w-full rounded-3xl p-5 bg-neutral-50 border border-neutral-100">
+                    <img src="public/LogOut.png" class="w-32 h-32" />
+                    <div class="flex flex-col justify-center items-center w-10/12">
+                        <p class="text-2xl font-semibold mt-3">Are You Sure?</p>
+                        <div class="flex gap-4 mt-3 w-11/12">
+                            <a href="logout.php" class="w-1/2 text-center px-4 py-1 rounded-xl bg-blue-600 text-zinc-100 font-semibold">Yes</a>
+                            <a x-on:click="open = false" href="#" class="font-semibold w-1/2 border-zinc-400 border-2 px-4 rounded-xl text-center">No</a>
                         </div>
                     </div>
                 </div>
