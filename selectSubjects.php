5ca6d93530c0768826d24b57ea8b2aecda94300a
Ifeoluwa <iamsamnice@gmail.com>
2025-11-05 09:14:27 +0100

    [FEAT]: LogOut Dialog Box Ui

--- selectSubjects.php
+++ selectSubjects.php
@@ -44,7 +44,7 @@ function getSelectedSubjects($conn, $student_id) {
             <div class="w-1/2">
                 <img src="public/BookLover.png" class="" />
             </div>
-            <div class="w-1/2 flex flex-col justify-center gap-6 p-3 border">
+            <div class="w-1/2 flex flex-col justify-center gap-6 p-3">
                 <div>
                     <h2 class="text-neutral-900 text-4xl font-semibold">Welcome <?= htmlspecialchars($_SESSION['student_name']) ?></h2>
                     <p class="text-zinc-400 font-semibold">Select your Offered Subjects...</p>
@@ -78,10 +78,10 @@ function getSelectedSubjects($conn, $student_id) {
                 <div class="flex flex-col items-center w-full rounded-3xl p-5 bg-neutral-50 border border-neutral-100">
                     <img src="public/LogOut.png" class="w-32 h-32" />
                     <div class="flex flex-col justify-center items-center w-10/12">
-                        <p class="text-2xl font-semibold mt-3">Are You Sure?</p>
+                        <p class="text-2xl font-semibold mt-3 text-neutral-800">Are You Sure?</p>
                         <div class="flex gap-4 mt-3 w-11/12">
-                            <a href="logout.php" class="w-1/2 text-center px-4 py-1 rounded-xl bg-blue-600 text-zinc-100 font-semibold">Yes</a>
-                            <a x-on:click="open = false" href="#" class="font-semibold w-1/2 border-zinc-400 border-2 px-4 rounded-xl text-center">No</a>
+                            <a href="logout.php" class="w-1/2 text-center px-4 py-1 rounded-xl bg-blue-600 text-neutral-100 font-semibold">Yes</a>
+                            <a x-on:click="open = false" href="#" class="font-semibold w-1/2 border-zinc-400 border-2 px-4 rounded-xl text-center text-neutral-800">No</a>
                         </div>
                     </div>
                 </div>
