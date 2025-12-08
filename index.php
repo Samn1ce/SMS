0c392864dc49390cab6c807ba91cae43f5078e5a
Ifeoluwa <iamsamnice@gmail.com>
2025-12-08 12:55:24 +0100

    Refactor code

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>  -->
     <link href="./src/output.css" rel="stylesheet">
     <title>Document</title>
 </head>
